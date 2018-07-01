78218e101e88643f87f51b7dd512119cf1443563
Milton Leung <milleung964@gmail.com>
2018-06-30 22:56:49 -0700

    Added MVVM Module

---
+++ MVVM Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+final class ___VARIABLE_sceneName___ViewController: UIViewController {
+
+  var viewModel: ___VARIABLE_sceneName___ViewModel
+
+  init(viewModel: ___VARIABLE_sceneName___ViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ MVVM Module.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+protocol ___VARIABLE_sceneName___ViewModel {
+
+}
+
+final class ___VARIABLE_sceneName___ViewModelImpl {
+
+
+  // Coordinator Handlers
+
+
+  // View Controller Handlers
+
+}
+
+extension ___VARIABLE_sceneName___ViewModelImpl: ___VARIABLE_sceneName___ViewModel {
+
+}
