9455a2607206fee79ad0d5f89d015ea5cd4194b1
Milton Leung <milleung964@gmail.com>
2018-09-04 18:33:41 -0700

    Add collection view and table view cells

---
+++ TableViewCell.xctemplate/___FILEBASENAME___Cell.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+final class ___VARIABLE_sceneName___Cell: UITableViewCell {
+
+  var viewModel: ___VARIABLE_sceneName___CellViewModel!
+
+  func configure(viewModel: ___VARIABLE_sceneName___CellViewModel) {
+    self.viewModel = viewModel
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    applyStyling()
+  }
+
+  func applyStyling() {
+
+  }
+}
---
+++ TableViewCell.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+protocol ___VARIABLE_sceneName___CellViewModel {
+
+}
+
+final class ___VARIABLE_sceneName___CellViewModelImpl {
+
+
+  // Coordinator Handlers
+
+
+  // View Controller Handlers
+
+}
+
+extension ___VARIABLE_sceneName___CellViewModelImpl: ___VARIABLE_sceneName___CellViewModel {
+
+}
